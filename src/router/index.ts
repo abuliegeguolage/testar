0ae9004d217d9a1fba7521623222cca2b4314fc7
abuliegeguolage <123>
2024-02-11 23:20:49 +0800

    refactor: delete unused default files & installed dependencies

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,22 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Divination from '../views/Divination.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
+      redirect: '/divination'
+    },
+    {
+      path: '/',
       name: 'home',
       component: HomeView
+    },
+    {
+      path: '/divination',
+      component: Divination
     }
   ]
 })
